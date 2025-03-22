c04b814b9f47613f47af7c4c89d9589294e0a823
Greg Clayton <clayborg@gmail.com>
2025-03-21 18:27:36 -0700

    Fix dumping --dynamic section when there are some DT_ defines that we don't recognize.

--- elf.py
+++ elf.py
@@ -814,6 +814,24 @@ class DT(IntEnum):
     def max_width(cls):
         return 18
 
+    # We might parse DWARF with user defined attributes. We need to support
+    # displaying these unknown attributes.
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, int):
+            return cls._create_pseudo_member_(value)
+        return None # will raise the ValueError in Enum.__new__
+
+    @classmethod
+    def _create_pseudo_member_(cls, value):
+        pseudo_member = cls._value2member_map_.get(value, None)
+        if pseudo_member is None:
+            new_member = int.__new__(cls, value)
+            new_member._name_ = '%#4.4x' % value
+            new_member._value_ = value
+            pseudo_member = cls._value2member_map_.setdefault(value, new_member)
+        return pseudo_member
+
 # DT_FLAGS bits
 class DF(IntFlag):
     ORIGIN = 0x1
