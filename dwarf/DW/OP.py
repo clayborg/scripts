cb21373fedbbe14f27652c8323deb58964a973d1
Greg Clayton <clayborg@gmail.com>
2025-11-21 14:10:29 -0800

    Allow unknown DW_OP enums.

--- dwarf/DW/OP.py
+++ dwarf/DW/OP.py
@@ -177,3 +177,21 @@ class DW_OP(IntEnum):
 
     def __str__(self):
         return 'DW_OP_' + self.name.lower()
+
+    # We might parse DWARF with user defined attributes. We need to support
+    # displaying these unknown attributes.
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, int):
+            return cls.create_pseudo_member_(value)
+        return None # will raise the ValueError in Enum.__new__
+
+    @classmethod
+    def create_pseudo_member_(cls, value):
+        pseudo_member = cls._value2member_map_.get(value, None)
+        if pseudo_member is None:
+            new_member = int.__new__(cls, value)
+            new_member._name_ = '_unknown_%4.4x' % value
+            new_member._value_ = value
+            pseudo_member = cls._value2member_map_.setdefault(value, new_member)
+        return pseudo_member
