5ade79ea2872ba3d019c3acb8b232eb2d675c497
Greg Clayton <gclayton@fb.com>
2025-11-24 13:23:39 -0800

    Allow DW_INL to have invlaid values without raising an exception

--- dwarf/DW/INL.py
+++ dwarf/DW/INL.py
@@ -10,4 +10,22 @@ class DW_INL(IntEnum):
     declared_inlined = 0x03
 
     def __str__(self):
-        return 'DW_INL_' + self.name
+        return "DW_INL_" + self.name
+
+    # We might parse DWARF with user defined attributes. We need to support
+    # displaying these unknown attributes.
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, int):
+            return cls.create_pseudo_member_(value)
+        return None  # will raise the ValueError in Enum.__new__
+
+    @classmethod
+    def create_pseudo_member_(cls, value):
+        pseudo_member = cls._value2member_map_.get(value, None)
+        if pseudo_member is None:
+            new_member = int.__new__(cls, value)
+            new_member._name_ = "_unknown_%4.4x" % value
+            new_member._value_ = value
+            pseudo_member = cls._value2member_map_.setdefault(value, new_member)
+        return pseudo_member
